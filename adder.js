082f68064a476264eadb0c6c8b915a592f09b6ed
Julien Az <julien.azbrg@gmail.com>
2020-12-30 11:34:17 +0100

    typage

--- adder.js
+++
@@ -1,4 +0,0 @@
-const adder = (...nums) => {
-  return nums.reduce((total, num) => total + num, 0);
-};
-module.exports = adder;
--- sum.test.js
+++
@@ -1,6 +0,0 @@
-const sum = require("./sum");
-
-// premier paramètre décrit
-test("Additionner 2 nombres", () => {
-  expect(sum(1, 2)).toBe(3);
-});
